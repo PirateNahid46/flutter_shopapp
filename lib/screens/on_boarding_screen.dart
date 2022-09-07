71ef145d7261174534ce518b76ba986038a20f2a
PirateNahid <nhdnahid2022@gmail.com>
2022-09-07 20:13:41 +0600

    kk

--- lib/screens/on_boarding_screen.dart
+++ lib/screens/on_boarding_screen.dart
@@ -1,24 +1,95 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 
 
-class OnBoardingScreen extends StatelessWidget {
+class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({Key? key}) : super(key: key);
   static const String id = "onboard_screen";
 
   @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  double scrollPosition = 0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        onPageChanged: (val){
-      },
+      body: Stack(
         children: [
-          OnBoardPage(
-            boardImage: Image.asset('assets/images/1.png'),
+          PageView(
+            onPageChanged: (val){
+              setState(() {
+                scrollPosition = val.toDouble();
+              });
+          },
+            children: [
+              OnBoardPage(
+                boardColumn: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text("SHOP",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontFamily: 'Eagle'
+
+                      ),),
+                    SizedBox(
+                        height: 300,
+                        width: 300,
+                        child: Image.asset('assets/images/1.png')
+                    )
+                  ],
+                ),
+              ),
+              OnBoardPage(
+                boardColumn: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text("SHOP",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontFamily: 'Eagle'
+
+                      ),),
+                    SizedBox(
+                        height: 300,
+                        width: 300,
+                        child: Image.asset('assets/images/1.png')
+                    )
+                  ],
+                ),
+              ),
+
+
+            ],
           ),
-          OnBoardPage(
-            boardImage: Image.asset('assets/images/1.png'),
-          ),
-          
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              children: [
+                DotsIndicator(dotsCount: 5,
+                position: scrollPosition,
+                decorator: const DotsDecorator(
+                  activeColor: Colors.white,
+                  color: Colors.white10,
+                ),
+                ),
+                TextButton(
+                  child: const Text("SKIP TO APP >", style:
+                  TextStyle(
+                    fontFamily: 'Eagle',
+                    fontSize: 20,
+                  ),
+                  ),
+                  onPressed: (){},
+                ),
+                const SizedBox(height: 20,)
+              ],
+            ),
+          )
         ],
       ),
     );
@@ -26,13 +97,14 @@ class OnBoardingScreen extends StatelessWidget {
 }
 
 class OnBoardPage extends StatelessWidget {
-  final Image? boardImage;
-  const OnBoardPage({Key? key,this.boardImage}): super(key: key);
+  final Column? boardColumn;
+  const  OnBoardPage({Key? key, this.boardColumn}): super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: boardImage,
+      color: Colors.blueGrey,
+      child: Center(child: boardColumn,),
     );
   }
 }
